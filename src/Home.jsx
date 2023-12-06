b3bf0803d2d5cf42f277727e6a1784c1d43cb521
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-07 01:07:42 +0330

    Create search bar UI element
    Added a search bar to the user interface for future functionality. This commit focuses on the visual aspect of the search bar, without implementing its functionality.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,24 @@
+import { Input } from 'antd'
+import { AudioOutlined } from '@ant-design/icons'
+
+const { Search } = Input
+const suffix = (
+    <AudioOutlined
+      style={{
+        fontSize: 16,
+        color: '#1677ff',
+      }}
+    />
+  );
+
 function Home() {
-    return <></>
+    return (
+        <>
+            <div style={{ display: 'flex ', justifyContent: 'center', alignItems: 'center', height: '95vh'}}>
+                <Search placeholder='Search a Repository' allowClear suffix={suffix} enterButton style={{ width: '50%' }} />
+            </div>
+        </>
+    )
 }
 
 export default Home
