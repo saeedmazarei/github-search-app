c42c5cefbf416eaa3b119d2415c82a70c4a3ab01
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-07 18:15:48 +0330

    Fix API address URL
    In this commit, I fixed the URL used to make API requests in the searchRequest function.

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 import api from './config'
 
-export const searchRequest = (query) => api.get(`repositories?q=${query}`)
+export const searchRequest = (query) => api.get(`search/repositories?q=${query}`)
 
 export const searchDetail = (owner, repo) => api.get(`repos/${owner}/${repo}`)
