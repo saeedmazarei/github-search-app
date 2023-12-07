2e75a6e8170883c31997e814835c7a57cc5bc78a
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-07 22:01:09 +0330

    Move search result box to separate component and use Ant Design components to display the results

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { Input, Row, Col } from 'antd'
 import { AudioOutlined } from '@ant-design/icons'
 import { searchRequest } from '../services/apis'
+import ShowSearchResult from './ShowSearchResults'
 
 const { Search } = Input
 const suffix = (
@@ -19,13 +20,11 @@ function Home() {
     const [loading, setLoading] = useState(false)
 
     async function searchHandler(value) {
-        console.log(value)
         if (value === '') {
             setSearchResult([])
         } else {
             setLoading(true)
             const response = await searchRequest(value)
-            console.log(response.data.items)
             setSearchResult(response.data.items)
             setLoading(false)
         }
@@ -53,19 +52,8 @@ function Home() {
                         />
                     </Col>
                 </Row>
-                {console.log(searchResult.length)}
                 {searchResult.length !== 0 && (
-                    <Row justify={'center'} style={{marginTop: '10px'}}>
-                        <Col xs={20} sm={18} md={14} lg={12}>
-                            <div style={{ border: '1px solid black', height: '90vh', overflow: 'auto' }}>
-                                <ul>
-                                    {searchResult.map((value) => (
-                                        <li key={value.id}>{value.full_name}</li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </Col>
-                    </Row>
+                    <ShowSearchResult searchResult={searchResult} />
                 )}
             </div>
         </>
---
+++ src/components/ShowSearchResults.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+import { Row, Col, Avatar, List } from 'antd'
+
+function ShowSearchResult({ searchResult }) {
+    return (
+        <Row justify={'center'} style={{ marginTop: '10px' }}>
+            <Col xs={20} sm={18} md={14} lg={12}>
+                <div style={{ border: '1px solid black', height: '90vh', overflow: 'auto' }}>
+                    <List
+                        itemLayout="horizontal"
+                        dataSource={searchResult}
+                        renderItem={(item) => (
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={
+                                        <Avatar
+                                            src={item.owner.avatar_url}
+                                        />
+                                    }
+                                    title={<Link to={`/details/${item.owner.login}/${item.name}`}>{item.full_name}</Link>}
+                                    description={item.description}
+                                />
+                            </List.Item>
+                        )}
+                    />
+                </div>
+            </Col>
+        </Row>
+    )
+}
+
+ShowSearchResult.propTypes = {
+    searchResult: PropTypes.array.isRequired,
+}
+
+export default ShowSearchResult
