225b84eb0c0e173e0645ad0304646871797bca5c
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-08 14:55:46 +0330

    Add card component to display fetched repository data
    This commit adds a new card component that is responsible for displaying the fetched repository data and complete header of that

--- src/components/RepoDetails.jsx
+++ src/components/RepoDetails.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react'
 
 import { useParams } from 'react-router-dom'
-import { Skeleton } from 'antd'
+import { Avatar, Card, Space, Divider } from 'antd'
+import { ForkOutlined, EyeOutlined, CloseCircleOutlined } from '@ant-design/icons'
 
 import { searchDetail } from '../services/apis'
+
+const { Meta } = Card
 
 function RepoDetails() {
     const [loading, setLoading] = useState(false)
@@ -18,10 +21,43 @@ function RepoDetails() {
             setLoading(false)
         }
         getRepositoryDetail()
-    }, [owner,repo])
+    }, [owner, repo])
 
-    return <>{loading && <Skeleton active />}</>
+    return (
+        <>
+            <Card loading={loading} style={{ margin: '5%' }}>
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'flex-start',
+                        flexWrap: 'wrap'
+                    }}
+                >
+                    <Meta
+                        avatar={<Avatar src={repoDetail?.owner.avatar_url} />}
+                        title={repoDetail?.full_name}
+                        description={repoDetail?.description}
+                    />
+                    <Space split={<Divider type="vertical" />}>
+                        <Space>
+                            <ForkOutlined />
+                            <span>Fork: {repoDetail?.forks}</span>
+                        </Space>
+                        <Space>
+                            <EyeOutlined />
+                            <span>Watchers: {repoDetail?.watchers}</span>
+                        </Space>
+                        <Space>
+                            <CloseCircleOutlined />
+                            <span>Open Issues: {repoDetail?.open_issues}</span>
+                        </Space>
+                    </Space>
+                </div>
+                <Divider />
+            </Card>
+        </>
+    )
 }
 
 export default RepoDetails
-
