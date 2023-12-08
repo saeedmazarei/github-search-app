5316cdc407f72d342ef5e020678a93b8c1e127e1
sAeed <saeed@sAeeds-MacBook-Air.local>
2023-12-08 13:27:26 +0330

    Add Logo to home page and add some css to style it

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 
-import { Input, Row, Col } from 'antd'
+import { Input, Row, Col, Image } from 'antd'
 import { AudioOutlined } from '@ant-design/icons'
 import { searchRequest } from '../services/apis'
 import ShowSearchResult from './ShowSearchResults'
+import '../style/style.css'
 
 const { Search } = Input
 const suffix = (
@@ -32,16 +33,25 @@ function Home() {
 
     return (
         <>
-            <div
-                style={{
-                    height: '100vh',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                }}
-            >
-                <Row justify={'center'}>
-                    <Col xs={20} sm={18} md={14} lg={12}>
+            <div className="main-container">
+                {searchResult.length === 0 && (
+                    <Row justify="center">
+                        <Col xs={24} sm={18} md={14} lg={12}>
+                            <Image src="/assets/logo.png" className="logo" preview={false} />
+                        </Col>
+                    </Row>
+                )}
+                <Row justify="center" align="top">
+                    {searchResult.length !== 0 && (
+                        <Col xs={3} sm={2} md={2} lg={1}>
+                            <Image
+                                src="/assets/logo.png"
+                                className='small-logo'
+                                preview={false}
+                            />
+                        </Col>
+                    )}
+                    <Col xs={17} sm={17} md={13} lg={11}>
                         <Search
                             placeholder="Search a Repository"
                             allowClear
@@ -52,9 +62,7 @@ function Home() {
                         />
                     </Col>
                 </Row>
-                {searchResult.length !== 0 && (
-                    <ShowSearchResult searchResult={searchResult} />
-                )}
+                {searchResult.length !== 0 && <ShowSearchResult searchResult={searchResult} />}
             </div>
         </>
     )
